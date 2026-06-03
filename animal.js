d242092580dcb14ba0d0e7b39b1c5865e36f8fa9
junio <rogerio.junior30@escola.pr.gov.br>
2026-06-03 13:53:03 +0000

    v1

--- animal.js
+++ animal.js
@@ -1,1 +1,10 @@
+const animal = {
+    nome: "TikiTiki", //string
+    especie: "Lagosta", //string
+    idade: 4, //number
+    tutor: false,
+    vacinado: true
+}    
 
+//console.log(animal);
+console.log(animal.nome);
